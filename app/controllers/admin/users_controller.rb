e59ff8bced6535899d047a2defad4e8924dde99f
EC2 Default User <ec2-user@ip-172-31-31-94.ec2.internal>
2022-09-15 09:45:16 +0000

    [add]admin/user/edit/show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
-   before_action :set_user, only: [:show, :edit, :update]
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -14,11 +14,30 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_root_path, notice: 'ユーザーの情報を編集しました'
+    else
+      flash.now[:alert] = '編集に失敗しました'
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+       redirect_to admin_root_path, notice: 'ユーザーを削除しました'
+    else
+      flash.now[:alert] = '削除に失敗しました'
+      render admin_user_path(@user)
+    end
   end
 
   private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :profile_image, :is_active)
     end
 end
