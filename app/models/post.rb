9780708fef1e385146865095bcafcb4d29f820d0
EC2 Default User <ec2-user@ip-172-31-31-94.ec2.internal>
2022-09-28 10:21:50 +0000

    [add]layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,9 @@ class Post < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :notifications, dependent: :destroy
+  
+  
+  
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
