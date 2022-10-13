bed935b172b00f95e69c7bc58d56ebcdb12755e9
hayato2022 <gaomusunxiang@gmail.com>
2022-10-13 10:27:26 +0000

    [add]テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 admin = Admin.new(:email => 'chari_mate@hoge.com', :password => 'hogehoge')
 admin.save!
+
+users = User.create!(
+  [
+    {email: 'olivia@test.com', name: 'Olivia', password: 'password', is_active: true, introduction: 'こんにちは', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'james@test.com', name: 'James', password: 'password',is_active: false, introduction: 'よろしくお願いします', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'lucas@test.com', name: 'Lucas', password: 'password',is_active: true, introduction: 'よろしく', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
+
+posts = Post.create!(
+  [
+    {body: 'きれい', lat: 35.331592, lng:136.0728135, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"),  user_id: users[0].id },
+    {body: '自転車仲間',lat: 35.331592, lng:136.0728135, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"),  user_id: users[1].id},
+    {body: '桜', lat: 35.32599002244017, lng:135.98612450463867, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), user_id: users[2].id }
+  ]
+)
+
+
